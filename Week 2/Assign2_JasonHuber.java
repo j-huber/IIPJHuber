691015cbd9f6c99230731466e477f425c90a18ba
j-huber <jason.huber00@gmail.com>
2019-02-11 17:16:27 -0500

    Update Assign2_JasonHuber.java

--- Week 2/Assign2_JasonHuber.java
+++ Week 2/Assign2_JasonHuber.java
@@ -22,27 +22,43 @@ while (i<10)
 // problem 3
 var sentence = "I am creating this sentence to make use of all the silly vowels multiple times.";
 var s_length = sentence.length;
-var current_position=0, number_vowels = 0;
+var current_position=0, number_vowels = 0, num_A = 0, num_E = 0, num_I = 0, num_O = 0, num_U = 0, num_Y = 0;
 while (current_position <= s_length)
 {
     switch (sentence.substr(current_position,1))
     {
     case "a": 
-    case "A": 
+    case "A": num_A+=1;
+    number_vowels+=1;
+        break;
     case "e": 
-    case "E": 
+    case "E": num_E+=1;
+    number_vowels+=1;
+        break;
     case "i": 
-    case "I":
+    case "I": num_I+=1;
+    number_vowels+=1;
+        break;
     case "o": 
-    case "O": 
+    case "O": num_O+=1;
+    number_vowels+=1;
+        break;
     case "u": 
-    case "U": 
+    case "U": num_U+=1;
+    number_vowels+=1;
+        break;
     case "y": 
-    case "Y": 
+    case "Y": num_Y+=1;
         number_vowels+=1;
         break;
     }
      current_position++;
 }
-console.log(number_vowels);
-
+console.log("The sample sentence is : " + sentence);
+console.log("Number of A's: " + num_A);
+console.log("Number of E's: " + num_E);
+console.log("Number of I's: " + num_I);
+console.log("Number of O's: " + num_O);
+console.log("Number of U's: " + num_U);
+console.log("Number of Y's: " + num_Y);
+console.log("The total number of vowels is : " + number_vowels);
